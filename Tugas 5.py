2a0748659a0e7272fda436649b0ca7556b6d2ace
NadiraRamadhani <email@anda.com>
2023-10-12 16:01:39 +0700

    Commit pertama

---
+++ Tugas 5.py
@@ -0,0 +1,37 @@
+totalbelanja = float(input("Masukkan Total Belanja: "))
+if totalbelanja > 1000000:
+    print("Anda mendapatkan potongan member platinum sebesar 10%")
+    totalbelanja_diformat = '{:,}'.format(totalbelanja)
+    print("Total Belanja anda adalah: ", totalbelanja_diformat)
+    potongan = 0.1 * totalbelanja
+    potongan_diformat = '{:,}'.format(potongan)
+    print("Potongan harga anda sebanyak: ", potongan_diformat)
+    totalbayar = totalbelanja - potongan
+    totalbayar_diformat = '{:,}'.format(totalbayar)
+    print("Total bayar anda adalah: ", totalbayar_diformat)
+elif totalbelanja > 500000:
+    print("Anda mendapatkan potongan member platinum sebesar 5%")
+    totalbelanja_diformat = '{:,}'.format(totalbelanja)
+    print("Total Belanja anda adalah: ", totalbelanja_diformat)
+    potongan = 0.05 * totalbelanja
+    potongan_diformat = '{:,}'.format(potongan)
+    print("Potongan harga anda sebanyak: ", potongan_diformat)
+    totalbayar = totalbelanja - potongan
+    totalbayar_diformat = '{:,}'.format(totalbayar)
+    print("Total bayar anda adalah: ", totalbayar_diformat)
+elif totalbelanja > 200000:
+    print("Anda mendapatkan potongan member silver sebesar 2%")
+    totalbelanja_diformat = '{:,}'.format(totalbelanja)
+    print("Total Belanja anda adalah: ", totalbelanja_diformat)
+    potongan = 0.02 * totalbelanja
+    potongan_diformat = '{:,}'.format(potongan)
+    print("Potongan harga anda sebanyak: ", potongan_diformat)
+    totalbayar = totalbelanja - potongan
+    totalbayar_diformat = '{:,}'.format(totalbayar)
+    print("Total bayar anda adalah: ", totalbayar_diformat)
+else:
+    print("Total belanja anda masih kurang untuk mendapatkan diskon member")
+    totalbelanja_diformat = '{:,}'.format(totalbelanja)
+    print("Total Belanja anda adalah: ", totalbelanja_diformat)
+    
+    #Diformat untuk memudahkan dalam membaca
